6ac7234ac6e02421d838d345984afef44c0c0ec5
slightgrey <slightgrey@gmail.com>
2025-08-18 15:29:27 +0800

    dev: added swiper depencies for the category block

--- theme/assets/main.js
+++ theme/assets/main.js
@@ -1,9 +1,19 @@
-import './styles/main.css'
-import.meta.glob('../blocks/**/*.css', { eager: true })
-import Alpine from 'alpinejs'
+import "./styles/main.css";
+import.meta.glob("../blocks/**/*.css", { eager: true });
+import Alpine from "alpinejs";
 
-window.Alpine = Alpine
+// Import Swiper and modules globally
+import { Swiper } from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
 
-import.meta.glob('../blocks/**/*.js', { eager: true })
+// Make Swiper globally available
+window.Swiper = Swiper;
+window.SwiperNavigation = Navigation;
 
-window.Alpine.start()
+window.Alpine = Alpine;
+
+import.meta.glob("../blocks/**/*.js", { eager: true });
+
+window.Alpine.start();
