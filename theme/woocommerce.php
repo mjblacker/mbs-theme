1da8a5e788b62bc8d28b8ac5cb5e551db8ca8f57
slightgrey <slightgrey@gmail.com>
2025-08-19 14:13:37 +0800

    dev: added check  if woo plugin is installed and hide cart icon on nav to prevent error

--- theme/woocommerce.php
+++ theme/woocommerce.php
@@ -7,6 +7,16 @@
  * @since Timberland 2.2.0
  */
 
+// Check if WooCommerce is active before proceeding
+if ( ! function_exists( 'WC' ) || ! function_exists( 'wc_get_product' ) ) {
+    // If WooCommerce is not active, redirect to 404 or home page
+    global $wp_query;
+    $wp_query->set_404();
+    status_header( 404 );
+    get_template_part( '404' );
+    return;
+}
+
 $context = Timber::context();
 
 if ( is_singular( 'product' ) ) {
