2213d2840d4c6760340db3a839f49dd5cf682784
slightgrey <slightgrey@gmail.com>
2025-08-22 15:16:09 +0800

    dev: removed all styles in style.css, loaded the wp-block-library in the frontend for gutenberg block styling, Used tailwind classes in all blocks

--- theme/app/Sapling/Sapling.php
+++ theme/app/Sapling/Sapling.php
@@ -65,7 +65,7 @@ class Sapling extends \Timber\Site
 
     public function enqueue_assets()
     {
-        wp_dequeue_style('wp-block-library');
+        // wp_dequeue_style('wp-block-library');
         wp_dequeue_style('wp-block-library-theme');
         wp_dequeue_style('wc-block-style');
         wp_dequeue_script('jquery');
