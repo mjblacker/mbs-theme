83781dceefc171dcd97847f5799e30d7be39f1e0
Matt Blacker <mattjblacker@gmail.com>
2025-09-16 13:40:52 +1000

    Feat: Alpine plugins for alert banner and location popover upgrades

--- theme/assets/main.js
+++ theme/assets/main.js
@@ -1,7 +1,8 @@
 import "./styles/main.css";
 import.meta.glob("../blocks/**/*.css", { eager: true });
-import Alpine from "alpinejs";
-
+import Alpine from "alpinejs"
+import focus from '@alpinejs/focus'
+import persist from '@alpinejs/persist'
 
 // Import Swiper and modules globally
 import { Swiper } from "swiper";
@@ -13,7 +14,13 @@ import "swiper/css/navigation";
 window.Swiper = Swiper;
 window.SwiperNavigation = Navigation;
 
+// Import alpinejs modules for this site
+import alertPersist from './js/alpinejs-alert-persist.js'
+
+Alpine.plugin(focus)
+Alpine.plugin(persist)
 window.Alpine = Alpine;
+window.AlertPersist = alertPersist;
 
 import.meta.glob("../blocks/**/*.js", { eager: true });
 import.meta.glob("./js/**/*.js", { eager: true });
